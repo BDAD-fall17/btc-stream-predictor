98b38e5a75f9297a2ef8f17b1b8842abe6a9709a
u932b <curiojustus@gmx.com>
2017-11-27 19:45:37 -0500

    trying to modulize model

---
+++ src/main/scala/naiveBayesModel2.scala
@@ -0,0 +1,82 @@
+import org.apache.spark._
+import org.apache.spark.mllib.classification.{NaiveBayes, NaiveBayesModel}
+import org.apache.spark.sql._
+import org.apache.spark.sql.Row
+import org.apache.spark.sql.types.{StructType, StructField, StringType, IntegerType,DoubleType};
+import org.apache.spark.mllib.feature.HashingTF
+import org.apache.spark.mllib.regression.LabeledPoint
+import org.apache.spark.mllib.linalg.Vector
+
+
+object Utils {
+  val hashingTF = new HashingTF(1000)
+
+  def transformFeatures(tweetText: Seq[String]): Vector = {
+    hashingTF.transform(tweetText)
+  }
+}
+
+// import com.typesafe.config.{Config, ConfigFactory}
+object PropertiesLoader {
+  // private val conf: Config = ConfigFactory.load("application.conf")
+  // val nltkStopWords = conf.getString("NLTK_STOPWORDS_FILE_NAME ")
+  val nltkStopWords = "NLTK_English_Stopwords_Corpus.txt"
+}
+
+object tweetTool{
+  def getBarebonesTweetText(tweetText: String, stopWordsList: List[String]): Seq[String] = {
+     //Remove URLs, RT, MT and other redundant chars / strings from the tweets.
+     tweetText.toLowerCase()
+       .replaceAll("\n", "")
+       .replaceAll("rt\\s+", "")
+       .replaceAll("\\s+@\\w+", "")
+       .replaceAll("@\\w+", "")
+       .replaceAll("\\s+#\\w+", "")
+       .replaceAll("#\\w+", "")
+       .replaceAll("(?:https?|http?)://[\\w/%.-]+", "")
+       .replaceAll("(?:https?|http?)://[\\w/%.-]+\\s+", "")
+       .replaceAll("(?:https?|http?)//[\\w/%.-]+\\s+", "")
+       .replaceAll("(?:https?|http?)//[\\w/%.-]+", "")
+       .split("\\W+")
+       .filter(_.matches("^[a-zA-Z]+$"))
+       .filter(!stopWordsList.contains(_))
+  }
+}
+object naiveBayesModel {
+  def main(args: Array[String]) {
+    val customSchema_1 = StructType(Array(
+        StructField("TIME", StringType, true),
+        StructField("tweet", StringType, true),
+        StructField("score", StringType, true))
+    );
+
+
+    val rawData = sc.textFile("file:///home/chc631/sparkClass/project/tweetwise_data/tweets_with_sentiment.txt")
+    val rowRDD = rawData.map(line => Row.fromSeq(line.split("\\|\\|")))
+    val tweetsDF = spark.createDataFrame(rowRDD, customSchema_1)
+    val tweetsDF2 = tweetsDF.selectExpr("TIME", 
+                        "tweet",
+                        "cast(score as double) score" 
+                        )
+
+
+    val stopWords = sc.textFile("file:////home/chc631/sparkClass/project/btc-stream-predictor/src/main/resources/NLTK_English_Stopwords_Corpus.txt")
+    val stopWordsList = stopWords.collect.toList
+
+    val labeledRDD = tweetsDF2.select("score", "tweet").rdd.map {
+      case Row(score: Double, tweet: String) =>
+      val tweetInWords: Seq[String] = tweetTool.getBarebonesTweetText(tweet, stopWordsList)
+      LabeledPoint(score, Utils.transformFeatures(tweetInWords))
+    }
+    labeledRDD.cache()
+
+    val naiveBayesModel = NaiveBayes.train(labeledRDD, lambda = 1.0, modelType = "multinomial")
+    // naiveBayesModel.save(sc, PropertiesLoader.naiveBayesModelPath)
+    naiveBayesModel.save(sc, "target/tmp/NaiveBayesModel")
+
+    val test = tweetTool.getBarebonesTweetText("bitcoin is so good i should buy but i am shit at buying", stopWordsList)
+
+    naiveBayesModel.predict(Utils.transformFeatures(test))
+
+  }
+}
