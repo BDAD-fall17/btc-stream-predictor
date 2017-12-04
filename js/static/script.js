c19d6e03d9f40b31207fbea2a5f25e7e1f94439a
pranaypareek <pranay.metal@gmail.com>
2017-12-03 20:19:06 -0500

    adds d3 graphs

---
+++ js/static/script.js
@@ -0,0 +1,7 @@
+function main() {
+    document.getElementById('iframe_chart_1').contentWindow.location.reload(true);
+    document.getElementById('iframe_chart_2').contentWindow.location.reload(true);
+    document.getElementById('iframe_chart_3').contentWindow.location.reload(true);
+}
+
+document.addEventListener("DOMContentLoaded", main);
